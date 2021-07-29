615b4ce59909018cc1010ac5a5db6a46f292b255
Jonathan du Mesnil <jonathan.mesnil@rwth-aachen.de>
2021-07-29 14:45:19 +0200

    add minimal example for dash api

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+import dash
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.H1(children="Willkommen zu Dash!")
+
 if __name__ == "__main__":
-  pass
+    app.run_server(debug=True)
